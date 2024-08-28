4388be27bfccac2c2db4d648888c795a2e716296
Tapan Sharma <tapansharma69@gmail.com>
2024-08-28 16:46:00 +0545

    applying conditional classes

--- resources/views/livewire/chat/message.blade.php
+++ resources/views/livewire/chat/message.blade.php
@@ -1,11 +1,26 @@
 <div>
     <!-- Messages Received -->
-    <div class="flex w-5/6 flex-col gap-2 lg:w-2/3 xl:w-1/3 items-start">
-        <p class="text-sm font-medium text-slate-500">
-            {{ $message->user->name }}
-        </p>
-        <div class="rounded-2xl px-5 py-3 rounded-br-none bg-gray-100">
-            <p class="font-semibold text-slate-600">
+    <div @class([
+        'flex w-5/6 flex-col gap-2 lg:w-2/3 xl:w-1/3',
+        'items-start' => $message->user_id !== auth()->id(),
+        'items-end ms-auto' => $message->user_id === auth()->id(),
+    ])>
+        @if ($message->user_id != auth()->id())
+            <p class="text-sm font-medium text-slate-500">
+                {{ $message->user->name }}
+            </p>
+        @endif
+        <div @class([
+            'rounded-2xl px-5 py-3',
+            'bg-indigo-600 text-white rounded-br-none' =>
+                $message->user_id == auth()->id(),
+            'bg-gray-100 rounded-tl-none' => $message->user_id != auth()->id(),
+        ])>
+            <p @class([
+                'font-semibold',
+                'text-white' => $message->user_id == auth()->id(),
+                'text-slate-500' => $message->user_id != auth()->id(),
+            ])>
                 {{ $message->body }}
             </p>
         </div>
@@ -14,17 +29,4 @@
         </p>
     </div>
     <!-- END  Messages Received -->
-
-    <!-- Messages Sent -->
-    {{-- <div class="flex w-5/6 flex-col gap-2 lg:w-2/3 xl:w-1/3 items-end ms-auto">
-        <div class="rounded-2xl px-5 py-3 rounded-tl-none bg-indigo-600">
-            <p class="font-semibold text-white">
-                Message Content
-            </p>
-        </div>
-        <p class="text-xs font-medium text-slate-500 text-right">
-            16:25 am
-        </p>
-    </div> --}}
-    <!-- END Messages Sent -->
 </div>
